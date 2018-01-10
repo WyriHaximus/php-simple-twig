2ebd318487b3c71d05b313dfdb962fbd2b885fc2
Cees-Jan Kiewiet <ceesjank@gmail.com>
2018-01-10 07:52:28 +0100

    Use lang unlikely to be used name for the content placeholder and template name

--- src/functions.php
+++ src/functions.php
@@ -6,6 +6,11 @@ use Twig_Environment;
 use Twig_Extension_StringLoader;
 use Twig_Loader_Array;
 
+/**
+ * Do not use this name in your data array as it will be overwritten
+ */
+const NAME_AND_PLACEHOLDER = '_______WyriHaximus_Twig_Render_template_contents_______';
+
 function render(string $template, array $data): string
 {
     return renderWithEnvironment($template, $data, createEnvironment());
@@ -13,16 +18,16 @@ function render(string $template, array $data): string
 
 function renderWithEnvironment(string $template, array $data, Twig_Environment $environment): string
 {
-    $data['string'] = $template;
+    $data[NAME_AND_PLACEHOLDER] = $template;
 
-    return $environment->render('string', $data);
+    return $environment->render(NAME_AND_PLACEHOLDER, $data);
 }
 
 function createEnvironment(): Twig_Environment
 {
     $environment = new Twig_Environment(
         new Twig_Loader_Array([
-            'string' => '{{ include(template_from_string(string)) }}',
+            NAME_AND_PLACEHOLDER => '{{ include(template_from_string(' . NAME_AND_PLACEHOLDER . ')) }}',
         ])
     );
     $environment->addExtension(new Twig_Extension_StringLoader());
